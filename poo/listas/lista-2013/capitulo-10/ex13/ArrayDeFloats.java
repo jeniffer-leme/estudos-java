27606eded79ac9fd5ad00a10ea150153f9152db8
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-01-13 23:06:46 -0300

    Create ArrayDeFloats.java

---
+++ poo/listas/lista-2013/capitulo-10/ex13/ArrayDeFloats.java
@@ -0,0 +1,68 @@
+public class ArrayDeFloats {
+    private float[] array;
+
+    public ArrayDeFloats(int numeros) {
+        array = new float[numeros];
+    }
+
+    public ArrayDeFloats(int numeros, int numConstante) {
+        array = new float[numeros];
+        
+        for(int c = 0; c < array.length; c++) {
+            array[c] = numConstante;
+        }
+    }
+    
+    public float[] getArray() {
+        return array;
+    }
+
+    public void setArray(float[] array) {
+        this.array = array;
+    }
+    
+    public int tamanho() {
+         return array.length;
+    }
+    
+    public void modifica(int posicao, float valor) {
+        if((posicao >= 0) && (posicao < array.length)) {
+            array[posicao] = valor;
+        }
+    }
+    
+    public float valor(int posicao) {
+        if((posicao >= 0) && (posicao < array.length)) {
+            return array[posicao];
+        } else {
+            return Float.NaN;
+        }
+    }
+    
+    public float[] paraArray() {
+        return array;
+    }
+    
+    public float menorValor() {
+        float menorAteAgora = array[0];
+        for(int c = 1; c < array.length; c++) {
+            if(array[c] < menorAteAgora) {
+                menorAteAgora = array[c];
+            }
+        }
+        return menorAteAgora;
+    }
+    
+    public boolean ehIgual(ArrayDeFloats outro) {
+        if(this.array.length != outro.array.length) {
+            return false;
+        }
+        
+        for(int i = 0; i < this.array.length; i++) {
+            if(this.array[i] != outro.array[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
