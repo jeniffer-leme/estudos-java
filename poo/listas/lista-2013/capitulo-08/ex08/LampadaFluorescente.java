0faabd55ef134ef8b48374a0d02d5717c5402c0b
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-01-13 22:37:29 -0300

    Create LampadaFluorescente.java

---
+++ poo/listas/lista-2013/capitulo-08/ex08/LampadaFluorescente.java
@@ -0,0 +1,20 @@
+public class LampadaFluorescente extends Lampada{
+    private int centimetros;
+
+    public LampadaFluorescente(boolean estadoLampada, int centimentros) {
+        super(estadoLampada);
+        setCentimetros(centimetros);
+    }
+
+    public int getCentimetros() {
+        return centimetros;
+    }
+
+    public void setCentimetros(int centimetros) {
+        this.centimetros = centimetros;
+    }
+    
+    public void tamanhoLampada(){
+        System.out.println("A lampada mede " + getCentimetros() + " cm.");
+    }
+}
