8eea083b27c35a0dc5ac8e6d440f4abb29f5cfea
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-01-13 22:49:30 -0300

    Update LivroLivraria.java

--- poo/listas/lista-2013/capitulo-08/ex19/LivroLivraria.java
+++ poo/listas/lista-2013/capitulo-08/ex19/LivroLivraria.java
@@ -2,7 +2,7 @@ public class LivroLivraria extends Livro {
     private double preco;
     private int estoque;
 
-    public LivroLivraria(String nome, String autor, int paginas, double preco, int estoque, int codigo) {
+    public LivroLivraria(String nome, String autor, int paginas, double preco, int estoque) {
         super(nome, autor, paginas);
         setEstoque(estoque);
     }
