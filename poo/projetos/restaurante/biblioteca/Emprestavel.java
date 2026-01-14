24ef59fd0a156cf08a01ca53d46e5bad69835ba4
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-01-14 01:01:49 -0300

    Create Emprestavel.java

---
+++ poo/projetos/restaurante/biblioteca/Emprestavel.java
@@ -0,0 +1,10 @@
+public interface Emprestavel {
+    
+    public String getNomeObjeto();
+    
+    public boolean emprestar();
+    
+    public boolean  podeEmprestar();
+    
+    public boolean devolver();
+}
