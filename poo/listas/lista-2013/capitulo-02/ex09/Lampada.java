d7405144f6ae54cecb9d809914736162213836c3
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-01-13 21:35:39 -0300

    Create Lampada.java

---
+++ poo/listas/lista-2013/capitulo-02/ex09/Lampada.java
@@ -0,0 +1,49 @@
+public class Lampada {
+    private boolean estadoLampada;
+    private int watts;
+
+    public Lampada(boolean estadoLampada, int watts) {
+        setEstadoLampada(estadoLampada);
+        setWatts(watts);
+    }
+
+    public boolean getEstadoLampada() {
+        return estadoLampada;
+    }
+
+    public void setEstadoLampada(boolean estadoLampada) {
+        this.estadoLampada = estadoLampada;
+    }
+
+    public int getWatts() {
+        return watts;
+    }
+
+    public void setWatts(int watts) {
+        this.watts = watts;
+    }
+    
+    public void acende(){
+        setEstadoLampada(true);
+    }
+    
+    public void apaga(){
+        setEstadoLampada(false);
+    }
+    
+    public boolean mostrarEstado(){
+        if(getEstadoLampada()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean ehEconomica(){
+        if(watts < 40){
+            return true;
+        } else {
+            return false;
+        }
+    } 
+}
