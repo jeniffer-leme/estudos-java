4f62e0a8832ce442c32abd6d0d93c6cb78edf3f5
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-01-13 23:17:02 -0300

    Create StringDNA.java

---
+++ poo/listas/lista-2013/capitulo-11/ex21/StringDNA.java
@@ -0,0 +1,65 @@
+public class StringDNA {
+    
+    private String dna; 
+    
+    public StringDNA(String entrada) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < entrada.length(); i++) {
+            char c = Character.toUpperCase(entrada.charAt(i));
+            if (c == 'A' || c == 'C' || c == 'G' || c == 'T') {
+                sb.append(c);
+            }
+        }
+        
+        this.dna = sb.toString();
+    }
+    
+    @Override
+    public String toString() {
+        return dna;
+    }
+    
+    public char charAt(int posicao) {
+        if (posicao < 0 || posicao >= dna.length()) {
+            throw new IndexOutOfBoundsException("Posição inválida!");
+        }
+        return dna.charAt(posicao);
+    }
+    
+    public int quantosA() { return contar('A'); }
+    public int quantosC() { return contar('C'); }
+    public int quantosG() { return contar('G'); }
+    public int quantosT() { return contar('T'); }
+    
+    public int length() {
+        return dna.length();
+    }
+
+    private int contar(char base) {
+        int cont = 0;
+        for (int i = 0; i < dna.length(); i++) {
+            if (dna.charAt(i) == base) cont++;
+        }
+        return cont;
+    }
+
+    public StringDNA reversoComplementar() {
+        StringBuilder complementar = new StringBuilder();
+        
+        for (int i = 0; i < dna.length(); i++) {
+            char c = dna.charAt(i);
+            switch (c) {
+                case 'A': complementar.append('T'); break;
+                case 'T': complementar.append('A'); break;
+                case 'C': complementar.append('G'); break;
+                case 'G': complementar.append('C'); break;
+            }
+        }
+        
+        complementar.reverse();
+        
+        return new StringDNA(complementar.toString());
+    }
+}
+
