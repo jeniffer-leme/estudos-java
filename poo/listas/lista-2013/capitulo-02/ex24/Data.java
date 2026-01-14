8ccaec720753150caf036cc36f4ca67fb9b821fa
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-01-13 21:46:08 -0300

    Create Data.java

---
+++ poo/listas/lista-2013/capitulo-02/ex24/Data.java
@@ -0,0 +1,41 @@
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Data(int dia, int mes, int ano) {
+        setDia(dia);
+        setMes(mes);
+        setAno(ano);
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+  
+    public void duplicarData(Data d2){
+      setDia(d2.getDia()); 
+      setMes(d2.getMes());
+      setAno(d2.getAno());
+    }
+}
