0a4c863ec1b3d16db0f88daff96cb13e9c070086
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-01-13 22:22:26 -0300

    Create ConversaoDeUnidadesDeArea.java

---
+++ poo/listas/lista-2013/capitulo-05/ex10/ConversaoDeUnidadesDeArea.java
@@ -0,0 +1,18 @@
+public class ConversaoDeUnidadesDeArea {
+  
+    public static double convertorMetrosPés(double metros){
+        return metros * 10.76;
+    }
+    
+    public static double convertorPesCent(double pes){
+        return pes * 929.0;
+    }
+    
+    public static double converterMilhasAcres(double milhas){
+        return milhas * 640.0;
+    }
+    
+    public static double converterAcrePes(double acres){
+        return acres * 43.560;
+    }
+}
