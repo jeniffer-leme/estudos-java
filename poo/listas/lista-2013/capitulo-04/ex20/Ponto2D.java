1a130dca169a5a9a1a7bf66e16e2b5e432101616
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-01-13 22:15:14 -0300

    Create Ponto2D.java

---
+++ poo/listas/lista-2013/capitulo-04/ex20/Ponto2D.java
@@ -0,0 +1,24 @@
+public class Ponto2D {
+    private double x, y;
+
+    public Ponto2D(double x, double y) {
+        setX(x);
+        setY(y);
+    }
+    
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
