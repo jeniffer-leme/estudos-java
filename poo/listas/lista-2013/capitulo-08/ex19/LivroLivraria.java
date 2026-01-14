fd46552a09d400cef71c1867ed2016a5c1eb3d3b
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-01-13 22:47:15 -0300

    Create LivroLivraria.java

---
+++ poo/listas/lista-2013/capitulo-08/ex19/LivroLivraria.java
@@ -0,0 +1,35 @@
+public class LivroLivraria extends Livro {
+    private double preco;
+    private int estoque;
+
+    public LivroLivraria(String nome, String autor, int paginas, double preco, int estoque, int codigo) {
+        super(nome, autor, paginas);
+        setEstoque(estoque);
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public boolean venderLivro(int quantidade){
+        if(estoque >= quantidade){
+            setEstoque(getEstoque() - quantidade);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+}
